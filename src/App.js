a91eb5af22351698c178a9b7586e76c55724b790
atgould <aarongouldlmt@gmail.com>
2022-09-24 15:16:48 -0400

    changed link

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://linkedin.com/aaron-gould-role-seeker"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          LinkedIn Profile
         </a>
       </header>
     </div>
